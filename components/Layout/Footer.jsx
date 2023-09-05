c639b886581ed52de0b79b7276af3ef0e6d14e98
unknown <jaafar.nasir71@gmail.com>
2023-09-05 05:32:31 +0100

    fix header link

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -2,6 +2,9 @@
 // import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 // import styles from './Footer.module.css';
 // import Spacer from './Spacer';
+
+import Link from "next/link";
+
 // import Wrapper from './Wrapper';
 
 const Footer = () => {
@@ -18,9 +21,11 @@ const Footer = () => {
       <footer className=" bg-green-50 bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{' '}
-          <a href="/sign-up" className="hover:underline">
+          <Link href="/sign-up" passHref>
+          <a  className="hover:underline">
             BAM-POWER
           </a>
+          </Link>
           . All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
